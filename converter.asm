8dc4fee23eae6bf7002abcf410501d0c0e900d73
EmiZanottiUNC <ezanotti@mi.unc.edu.ar>
2023-04-09 15:54:14 +0000

    Fixed EBP restoration

--- converter.asm
+++ converter.asm
@@ -2,11 +2,10 @@ segment .text
         global  asm_main
 asm_main:
         push ebp
-        mov ebp, esp
-        fld dword [ebp + 8]
-        fld dword [ebp + 12]
-        fmul ST1
+        mov ebp, esp ;Guardo valor original de ebp y cargo el valor de esp
+        fld dword [ebp + 8] ;cargo parametro 1 en unidad de punto flotante
+        fld dword [ebp + 12] ;cargo parametro 2 en unidad de punto flotante
+        fmul ST1 ; ST0 = ST0 * ST1
         
-        mov esp, ebp
-        pop ebp
+        pop ebp ;restauro ebp y salgo
         ret

f7d188b2ced8167cd31c5fe640b5d80f37ee29de
EvangelinaCastellano <evangelina.castellano@mi.unc.edu.ar>
2023-04-15 22:11:19 -0300

    Add coments

--- converter.asm
+++ converter.asm
@@ -2,10 +2,10 @@ segment .text
         global  asm_main
 asm_main:
         push ebp
-        mov ebp, esp ;Guardo valor original de ebp y cargo el valor de esp
-        fld dword [ebp + 8] ;cargo parametro 1 en unidad de punto flotante
-        fld dword [ebp + 12] ;cargo parametro 2 en unidad de punto flotante
-        fmul ST1 ; ST0 = ST0 * ST1
+        mov ebp, esp            ;Guardo valor original de ebp y cargo el valor de esp
+        fld dword [ebp + 8]     ;cargo parametro 1 en unidad de punto flotante ST0
+        fld dword [ebp + 12]    ;cargo parametro 2 en unidad de punto flotante ST1
+        fmul ST1                ;ST0 = ST0 * ST1
         
-        pop ebp ;restauro ebp y salgo
+        pop ebp                 ;Restauro ebp y salgo
         ret
--- tp2.py
+++ tp2.py
@@ -1,32 +1,35 @@
-import requests
-import ctypes
+import requests 
+import ctypes   
 
-def exchange(from_currency, to_currency):
+# Funcion que llama a la API para obtener el valor de cambio de cada moneda:
+def exchange(from_currency, to_currency): 
     data = requests.get(url.format(from_currency, to_currency)).json()
     if 'Note' in data:
         print("Limite de llamadas a API alcanzado. Intente de nuevo en 1 minuto.")
         quit()
     return float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
-try:
-    libconvert = ctypes.CDLL('./libconvert.so')
-except OSError:
-    print("Libreria 'libconvert.so' no encontrada. Ejecuto 'make'?")
-    quit()
-libconvert.convert.argtypes = (ctypes.c_float, ctypes.c_float)
-libconvert.convert.restype = (ctypes.c_float)
 
+# Funcion que utiliza la libreria externa para realizar la conversion:
 def convert(base, exchange):
     return libconvert.convert(base, exchange)
 
 apiKey = 'X7NY3G5XB9OWU75B'
 url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={}&to_currency={}&apikey=' + apiKey
 
+try:
+    libconvert = ctypes.CDLL('./libconvert.so') #Carga la libreria
+except OSError:
+    print("Libreria 'libconvert.so' no encontrada. Ejecuto 'make'?")
+    quit()
+
+libconvert.convert.argtypes = (ctypes.c_float, ctypes.c_float) #Carga los tipos de argumentos que recibe la funcion convert
+libconvert.convert.restype = (ctypes.c_float)                  #Carga el tipo de retorno de la funcion convert
+
 BTC = exchange('BTC', 'USD')
 ETH = exchange('ETH', 'USD')
 USDT = exchange('USDT', 'USD')
 ARS = exchange('USD', 'ARS')
 EUR = exchange('USD', 'EUR')
-
 
 print("Bitcoin: | USD: ", BTC, " | ARS: ", convert(BTC, ARS), " | EUR: ", convert(BTC, EUR))
 print("Ethereum: | USD: ", ETH, " | ARS: ", convert(ETH, ARS), " | EUR: ", convert(ETH, EUR))
